d5bf6cf159718841c1306deed36f23ec1e76e01a
N.pro <nareksmartpro@gmail.com>
2022-01-07 17:23:14 +0400

    for text2

--- forum.1.py
+++ forum.1.py
@@ -1,10 +1,16 @@
 my_l = [0,]
-for i in range(1,10,1):
-    my_l.append(i)
-    print(my_l)
+ # for i in range(1,10,1):
+ #     my_l.append(i)
+ #     print(my_l)
 l_2 = ["ad", "ba", "da",]
 l_3 = my_l.extend(l_2)
 print(my_l)
 z = len(my_l)
-for i in range(0,z,2):
+# for i in range(0,z,2):
+#     print(my_l[i])
+i=0
+while i<z:
     print(my_l[i])
+    i = i+2
+
+
