72eb9462a4e3468f92a31c76cdfa884a596213c8
N.pro <nareksmartpro@gmail.com>
2022-01-07 16:56:12 +0400

    for text

---
+++ forum.1.py
@@ -0,0 +1,10 @@
+my_l = [0,]
+for i in range(1,10,1):
+    my_l.append(i)
+    print(my_l)
+l_2 = ["ad", "ba", "da",]
+l_3 = my_l.extend(l_2)
+print(my_l)
+z = len(my_l)
+for i in range(0,z,2):
+    print(my_l[i])
---
+++ main.py
@@ -0,0 +1,5 @@
+a = int(input("a = "))
+b = int(input("b = "))
+c = (a + b) / 2
+print(c)
+print(type(c))
